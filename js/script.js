acc26b1f5b454577e7d1fc9e04b3218e61361ab0
ejiroifie <ejiroifie@gmail.com>
2019-06-15 20:55:07 -0400

    fetched data and stored into array as object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 function getAdvice() {
-   
+
+    var adviceList= [];
+
+    const url ="https://api.adviceslip.com/advice";
+    fetch(url).then (responseJson => {
+       // console.log(responseJson);
+       adviceList.push(responseJson)
+       console.log(adviceList)
+    });
 }
+
+
+// Dependency injection
+// Means you can define HTML elements inside Javascript and transfer them to html page
+
+//var adviceDiv = "<div>" +
